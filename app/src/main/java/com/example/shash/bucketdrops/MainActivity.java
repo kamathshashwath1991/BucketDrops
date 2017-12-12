de25948a5e772af670025b4cae240bbbc7e1e2af
kamathshashwath1991 <kamathshashwath1991@gmail.com>
2017-12-11 23:54:59 -0500

    Buttons and Fonts updated

--- app/src/main/java/com/example/shash/bucketdrops/MainActivity.java
+++ app/src/main/java/com/example/shash/bucketdrops/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.shash.bucketdrops;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 /**
  * Created by shash on 12/11/2017.
@@ -10,9 +16,27 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
+    }
+
+    private void initBackgroundImage(){
+        ImageView background = findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .into(background);
+
+    }
+
+    public void showDialog(View view) {
+        Toast.makeText(MainActivity.this,"Button was clicked",Toast.LENGTH_SHORT).show();
     }
 }
