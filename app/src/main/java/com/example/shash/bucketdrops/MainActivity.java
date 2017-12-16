d39e77523ea867c6d50e21661391e9d079adbcde
kamathshashwath1991 <kamathshashwath1991@gmail.com>
2017-12-15 22:14:08 -0500

    Created CustomViews and items for RecyclerView

--- app/src/main/java/com/example/shash/bucketdrops/MainActivity.java
+++ app/src/main/java/com/example/shash/bucketdrops/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     Toolbar mToolbar;
     Button mBtnAdd;
     Realm mRealm;
-
+    RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         initBackgroundImage();
         mRealm = Realm.getDefaultInstance();
+        mRecyclerView=findViewById(R.id.rv_drops);
+
 
         mBtnAdd.setOnClickListener(mBtnAddListener);
     }
